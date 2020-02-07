d9029cf86de3c392656394d4e97865afb95343af
ihor.kulpekin <201199ss>
2020-02-07 03:51:33 +0200

    done refactoring

--- src/ui_react/components/Header/Header.js
+++ src/ui_react/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import {fade} from "@material-ui/core";
+import {fade} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import InputBase from "@material-ui/core/InputBase";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import InputBase from '@material-ui/core/InputBase';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
