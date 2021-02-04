fc43c10736c7b6362cac848c9df186fa65aadb11
Ihor kulpekin <konstantin.boykov@pro-store.online>
2021-02-04 02:45:07 +0200

    changed OrdersContainer

--- src/ui_react/containers/OrdersContainer.js
+++ src/ui_react/containers/OrdersContainer.js
@@ -1,27 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
+import {compose, lifecycle, withProps} from "recompose";
 
 import {getOrders} from '../../redux/actions/actions';
 import ListOrders from '../components/ListOrders/ListOrders';
-
-class OrdersContainer extends Component {
-  componentDidMount() {
-    const {orders, getOrders} = this.props;
-    if (orders === null) {
-      getOrders();
-    }
-  }
-
-  render() {
-    const {orders} = this.props;
-    return (
-      <>
-        <ListOrders orders={orders}/>
-      </>
-    );
-  }
-}
 
 const mapStateToProps = state => ({
   orders: state.orders
@@ -29,8 +11,17 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {getOrders};
 
-OrdersContainer.propTypes = {
-  getOrders:PropTypes.func.isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(OrdersContainer)
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    withProps((props)=>({
+        orders: props.orders
+    })),
+    lifecycle({
+      componentDidMount(){
+          const {orders, getOrders} = this.props;
+          if (orders === null) {
+              getOrders();
+          }
+      }
+    })
+)(ListOrders)
