04f6335859d49be95cc8db0df5125d6e177633c7
Ihor kulpekin <konstantin.boykov@pro-store.online>
2021-02-04 03:18:50 +0200

    added recompose to OrdersPositionsContainer

--- src/ui_react/components/Header/Header.js
+++ src/ui_react/components/Header/Header.js
@@ -13,88 +13,88 @@ import {connect} from "react-redux";
 import {getOrders} from "../../../redux/actions/actions";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex'
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    root: {
+        display: 'flex'
     },
-    marginLeft: 800,
-    width: '100%',
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1
     },
-  }
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 800,
+        width: '100%',
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200,
+        },
+    }
 }));
 
 const Header = ({onChange, value}) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h4">
-            OrderList
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon/>
-            </div>
-            <InputBase value={value}
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }} onChange={onChange}
-              inputProps={{'aria-label': 'search'}}
-            />
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h4">
+                        OrderList
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase value={value}
+                                   placeholder="Search…"
+                                   classes={{
+                                       root: classes.inputRoot,
+                                       input: classes.inputInput,
+                                   }} onChange={onChange}
+                                   inputProps={{'aria-label': 'search'}}
+                        />
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 };
 
 Header.propTypes = {
-  onFilterChange:PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired
+    onFilterChange: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired
 };
 
 const enhance = compose(
     connect(undefined, {getOrders}),
-    withProps((props)=>({
-      getOrders:props.getOrders
+    withProps((props) => ({
+        getOrders: props.getOrders
     })),
     withState('value', 'setValue', ''),
     withHandlers({
-      onChange: ({setValue,getOrders}) =>(event) => {
-         setValue(event.target.value);
-         getOrders(event.target.value)
-      }
+        onChange: ({setValue, getOrders}) => (event) => {
+            setValue(event.target.value);
+            getOrders(event.target.value)
+        }
     })
 )
 
--- src/ui_react/containers/OrderPositionsContainer.js
+++ src/ui_react/containers/OrderPositionsContainer.js
@@ -1,38 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
 
 import {getOrderItems} from '../../redux/actions/actions';
 import OrderItem from '../components/ListOrders/OrderItem/OrderItem';
-
-class OrderPositionsContainer extends Component {
-
-  onToggle = (event, expanded) => {
-    const {order, positions, getOrderItems} = this.props;
-    if (expanded && !positions) {
-      getOrderItems(order.id);
-    }
-  };
-
-  render() {
-    const {order,positions} = this.props;
-    return (
-      <>
-        <OrderItem order={order} onToggle={this.onToggle}  positions={positions}/>
-      </>
-    )
-  }
-}
+import {compose, withHandlers, withProps} from "recompose";
 
 const mapStateToProps = (state, props) => ({
-  positions: state.orderPositions[props.order.id]
+    positions: state.orderPositions[props.order.id]
 });
 
 const mapDispatchToProps = {getOrderItems};
 
-OrderPositionsContainer.propTypes = {
-  order:PropTypes.object.isRequired,
-  getOrderItems:PropTypes.func.isRequired
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(OrderPositionsContainer)
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    withProps(({order, positions, getOrderItems}) => ({
+        order,
+        positions,
+        getOrderItems
+    })),
+    withHandlers({
+        onToggle: ({order, positions, getOrderItems}) => () => {
+            if (!positions) {
+                getOrderItems(order.id);
+            }
+        }
+    })
+)(OrderItem)
